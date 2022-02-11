c58bbe50f86da45ce01246a874d474568eced099
kamalkech <maroc.develop@gmail.com>
2022-02-11 12:03:12 +0100

    .

--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to server deploy with docker!' };
+    return { message: 'Welcome to server deploy with docker 3 !' };
   }
 }
